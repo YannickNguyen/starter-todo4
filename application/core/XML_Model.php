c8a8f611943d31d31f71e81969526e2284015ad6
Evan Chen <evan.c1995@gmail.com>
2017-11-16 17:14:52 -0800

    Task name not displaying and such

--- application/core/XML_Model.php
+++ application/core/XML_Model.php
@@ -71,8 +71,8 @@ class XML_Model extends Memory_Model {
      */
     protected function store() {
         $xmlstr = '<?xml version="1.0" encoding="UTF-8"?>
-      <tasks>
-      </tasks>
+<tasks>
+</tasks>
       ';
         $this->reindex();
         //---------------------
